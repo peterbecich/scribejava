c6446e39d66d90d38e00b3abb267b050ec0ebad2
Peter Becich <peterbecich@gmail.com>
2017-12-21 21:00:15 -0800

    oauth2 endpoints? untested
    https://developer.twitter.com/en/docs/basics/authentication/overview/application-only

--- scribejava-apis/src/main/java/com/github/scribejava/apis/TwitterApi20.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/TwitterApi20.java
@@ -3,26 +3,29 @@ package com.github.scribejava.apis;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
 public class TwitterApi20 extends DefaultApi20 {
+    /*
+      https://developer.twitter.com/en/docs/basics/authentication/overview/application-only
+     */
 
     protected TwitterApi20() {
     }
 
     private static class InstanceHolder {
-	private static final TwitterApi20 INSTANCE = new TwitterApi20();
+        private static final TwitterApi20 INSTANCE = new TwitterApi20();
     }
 
     public static TwitterApi20 instance() {
-	return InstanceHolder.INSTANCE;
+        return InstanceHolder.INSTANCE;
     }
 
     @Override
     public String getAccessTokenEndpoint() {
-	return "";
+        return "https://api.twitter.com/oauth2/token";
     }
 
     @Override
     protected String getAuthorizationBaseUrl() {
-	return "";
+        return "https://api.twitter.com/oauth/authorize";
     }
-    
+
 }
